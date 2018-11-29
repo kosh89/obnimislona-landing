64f890cc263c4757bc1148ea5390c1c0524d29a8
Sergey Vasilyev <kosh.goo@gmail.com>
2018-11-29 22:00:48 +0300

    adapt -short_menu

--- script/script.js
+++
@@ -1,9 +0,0 @@
-icon.onclick = function() {
-    const x = document.getElementById('hidden_menu');
-    if (x.className == "menu") {
-        x.className += " responsive";
-    } else {
-        //Свернуть меню обратно
-        x.className = "menu"; 
-    }
-}
