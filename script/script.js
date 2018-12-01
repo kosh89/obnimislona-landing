de1fcbb0430115b6a9c54da3224858cc7057255d
Sergey Vasilyev <kosh.goo@gmail.com>
2018-12-01 03:02:57 +0300

    sendmail

---
+++ script/script.js
@@ -0,0 +1,11 @@
+/* document.getElementById('button').onclick = showContactUs;
+
+function showContactUs() {
+    document.getElementById('hidden_contact_us').style.top = '10%';
+    document.getElementById('hidden_contact_us').style.boxShadow = '0 0 0 1000px rgba(0,0,0,0.5)';    
+}
+
+document.getElementById('hideContactUs').onclick = function() {
+    document.getElementById('hidden_contact_us').style.top = '-40%';
+    document.getElementById('hidden_contact_us').style.boxShadow = 'none';    
+} */
