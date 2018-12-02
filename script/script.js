bd4b1b7b7fc6908361e5ca2d13b2c172e6c2be6e
Sergey Vasilyev <kosh.goo@gmail.com>
2018-12-02 12:14:03 +0300

    modal fix

--- script/script.js
+++ script/script.js
@@ -1,16 +1,21 @@
+const timer = setTimeout(hideModal, 2000);
+
 document.getElementById('button').onclick = function() {
-    hideModal();
     document.getElementsByClassName('form-container')[0].style.top = '10%';
+    //Если уже отправили, то окно уедет наверх.
+    timer;
 }
 
+//кнопку "скрыть" хорошо бы заменить на клик вне формы
 document.getElementById('hideContactUs').onclick = function() {
     document.getElementsByClassName('form-container')[0].style.top = '-45%';
 }
 
 document.getElementsByClassName('form-container__btn')[0].onclick = function() {        
-    const timer = setTimeout(hideModal, 2000);        
+    timer;
 }
 
+//если форма ушла на почту, то окно уедет
 function hideModal() {
     if (document.getElementsByClassName('fields')[0].style.display == 'none') {
         document.getElementsByClassName('form-container')[0].style.top = '-45%';
