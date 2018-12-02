c981838e95fb6c2c0d417c820b91fd0f16e8d51d
Sergey Vasilyev <kosh.goo@gmail.com>
2018-12-02 11:18:53 +0300

    modal hide

--- script/script.js
+++ script/script.js
@@ -5,3 +5,11 @@ document.getElementById('button').onclick = function() {
 document.getElementById('hideContactUs').onclick = function() {
     document.getElementsByClassName('form-container')[0].style.top = '-45%';
 }
+
+document.getElementById('form-container__btn').onclick = function() {
+    const timer = setTimeout(dong, 2000);
+}
+
+function dong() {
+    document.getElementsByClassName('form-container')[0].style.top = '-45%';
+}
