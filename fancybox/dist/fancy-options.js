4357dd6dc38f44734d5d47edbc9642ffd348d754
Sergey Vasilyev <kosh.goo@gmail.com>
2018-12-08 14:01:09 +0300

    gallery

---
+++ fancybox/dist/fancy-options.js
@@ -0,0 +1,53 @@
+$('[data-fancybox="weddings"]').fancybox({
+    margin: [44, 0, 22, 0],
+    loop: true,
+    thumbs: {
+        autoStart: true,
+        axis: 'x'
+    },
+    buttons: [
+        "zoom",
+        //"share",
+        "slideShow",
+        //"fullScreen",
+        //"download",
+        "thumbs",
+        "close"
+    ]
+})
+
+$('[data-fancybox="children"]').fancybox({
+    margin: [44, 0, 22, 0],
+    loop: true,
+    thumbs: {
+        autoStart: true,
+        axis: 'x'
+    },
+    buttons: [
+        "zoom",
+        //"share",
+        "slideShow",
+        //"fullScreen",
+        //"download",
+        "thumbs",
+        "close"
+    ]
+})
+
+$('[data-fancybox="corporate"]').fancybox({
+    margin: [44, 0, 22, 0],
+    loop: true,
+    thumbs: {
+        autoStart: true,
+        axis: 'x'
+    },
+    buttons: [
+        "zoom",
+        //"share",
+        "slideShow",
+        //"fullScreen",
+        //"download",
+        "thumbs",
+        "close"
+    ]
+})
